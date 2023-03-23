f17a0b3239ca9eb98b0c87b911ea4153587e5a23
Ella Parker <ellapark@live.unc.edu>
2023-03-23 13:30:26 -0400

    added -j, -e, and -s tag functionality

--- cli.js
+++ cli.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+
 //shebang for Node
 //to run this, do  galosh.js 35.9 79.0558
 import moment from 'moment-timezone';
@@ -12,27 +13,43 @@ const args = minimist(process.argv.slice(2));
 //console.log(args);
 
 let helpText = `Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
--h            Show this help message and exit.
--n, -s        Latitude: N positive; S negative.
--e, -w        Longitude: E positive; W negative.
--z            Time zone: uses tz.guess() from moment-timezone by default.
--d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
--j            Echo pretty JSON from open-meteo API and exit.`
+  -h            Show this help message and exit.
+  -n, -s        Latitude: N positive; S negative.
+  -e, -w        Longitude: E positive; W negative.
+  -z            Time zone: uses tz.guess() from moment-timezone by default.
+  -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
+  -j            Echo pretty JSON from open-meteo API and exit.`
 
 if (args.h){
     console.log(helpText);
     process.exit(0);
 }
 
-//separating args
-const lattitude = args.n;
-//console.log(lattitude);
-const longitude = args.w;
+//must use let here bc const must be initialized outside of an if/else
+let lattitude;
+let longitude;
 
+//separating args, remember they can be under S and E as well!
+if (args.n){
+  lattitude = args.n;
+} else if (args.s){
+  lattitude = -args.s;
+}
+
+if (args.e){
+  longitude = args.e;
+} else if (args.w){
+  longitude = -args.w;
+}
+
+//didn't add a customizable timezone tag!
+if (args.z){
+  timezone = args.z;
+}
 
 // Make a request, also TODO, add previously saved args to url
 //problem: url is not reading my variable bc it is certainly a float
-const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + lattitude + '&longitude=' + longitude + '&hourly=temperature_2m');
+const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + lattitude + '&longitude=' + longitude + '&daily=precipitation_hours&current_weather=true&timezone=' + timezone);
 
 // Get the data from the request
 const data = await response.json();
@@ -47,6 +64,9 @@ if (days == 0) {
   console.log("tomorrow.")
 }
 
-//data.daily.precipitation_hours[0]
+//only log data IF there is a j tag
+if(args.j){
+  console.log(data);
+  process.exit(0);
+}
 
-console.log(data);
